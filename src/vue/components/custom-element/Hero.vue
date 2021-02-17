cac2f785a0848c90e301df02384e373b5e8e276e
Yuan1819 <ryanyupwork@gmail.com>
2021-02-17 10:03:31 -0800

    Added Hero Vue

---
+++ src/vue/components/custom-element/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Vue Component</h1>
+    <p>Shopify Prop: {{ settings.title }}</p>
+    <p>Store Value: </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  props: {
+    settings: Object,
+  },
+  computed: {
+    ...mapGetters("example", ["value"])
+  }
+};
+console.log("settings", props.settings)
+</script>
--- src/vue/components/layout.js
+++ src/vue/components/layout.js
@@ -2,8 +2,11 @@
 
 import ExampleAddToCart from "@vue/components/custom-element/ExampleAddToCart.vue";
 import ExampleCartCounter from "@vue/components/custom-element/ExampleCartCounter.vue";
+import Hero from "@vue/components/custom-element/Hero.vue";
+
 
 export default {
+  "hero-element": Hero,
   "theme-add-to-cart": ExampleAddToCart,
   "theme-cart-counter": ExampleCartCounter
 };
